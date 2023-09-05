55e528951faccb7948ce6d16f5af1be35e71a3f2
harishdharmavaram <44495956+harishdharmavaram@users.noreply.github.com>
2023-09-05 18:09:39 +0530

    Update test_snowflake_connector.py

--- test_snowflake_connector.py
+++ test_snowflake_connector.py
@@ -9,7 +9,7 @@ class SnowflakeConnection(unittest.TestCase):
     # Set up the Snowflake connection here, return the connection object
 
 
-    with open("secret_dev.json") as secrets_file:
+    with open("secret.json") as secrets_file:
         secrets = json.load(secrets_file)
 
         user = secrets["snowflake"]["snowflake_user"]
